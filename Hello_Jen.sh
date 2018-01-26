3be1533241ada0a39aa365061de16d747810732d
stillGLFM <34388839+stillGLFM@users.noreply.github.com>
2018-01-26 10:34:50 +0200

    Update Hello_Jen.sh

--- Hello_Jen.sh
+++ Hello_Jen.sh
@@ -1,11 +1,3 @@
 ﻿#!/bin/bash
 
-bot_token="499353565:AAFIQ7pIv60PfA2wTYie-yiFgtHP3DTHMfY"
-bot_url="https://api.telegram.org/bot$bot_token/sendMessage"
-chat_id="237604323"
-message=""
-
-#стандартное(короткое и информативное) сообщение
-message="123"
-
-curl -s -X POST "$bot_url" -d chat_id="$chat_id" -d text="$message"
+"Hello Jen!"
