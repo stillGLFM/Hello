69d5c552d58b12cdfdc979b85d983f817857892a
stillGLFM <pavluk.gleb@gmail.com>
2018-01-26 03:40:54 +0200

    first

---
+++ Hello_Jen.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/bash
+
+bot_token="499353565:AAFIQ7pIv60PfA2wTYie-yiFgtHP3DTHMfY"
+bot_url="https://api.telegram.org/bot$bot_token/sendMessage"
+chat_id="237604323"
+message=""
+
+#стандартное(короткое и информативное) сообщение
+message="Hello world!"
+
+curl -s -X POST "$bot_url" -d chat_id="$chat_id" -d text="$message"
